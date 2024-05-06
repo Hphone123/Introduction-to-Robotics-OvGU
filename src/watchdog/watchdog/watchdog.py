c3769b56503ddeea38cb07cb438644a845ce004a
Hannes Krüger <000hphone000@gmail.com>
2024-05-06 08:57:53 +0200

    complete task 1

--- src/watchdog/watchdog/watchdog.py
+++ src/watchdog/watchdog/watchdog.py
@@ -6,6 +6,9 @@ from std_msgs.msg import String
 
 class WatchdogNode(Node):
 
+    shouldTurn = True
+    shouldMove = True
+
     def __init__(self):
         super().__init__('watchdog')
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
@@ -14,13 +17,25 @@ class WatchdogNode(Node):
         self.get_logger().info('Watchdog node started')
 
     def cmd_callback(self, msg):
-        # this makes the turle go backwards
-        # (just so you know its working)
-        msg.linear.x = -1 * msg.linear.x
-        self.get_logger().info(f'msg.linear.x: {msg.linear.x}')
+
+        # Message contains all Information from the User input (linear -> Transformation, angular -> Rotation)
+        msg.linear.x = 1 * msg.linear.x
+
+        # Don't Turn if you shouldn't (Turing is only representat on z-Axis)
+        if (self.shouldTurn == False): 
+            msg.angular.z = 0.0
+
+            # Don't move if you shouldn't (Moving is x and y-Axis)
+        if (self.shouldMove == False): 
+            msg.linear._x = 0.0
+            msg.linear._y = 0.0
+        
+        # Plublishing Message Gives it to designated (next) Node -> cmd_vel-Node
         self.publisher.publish(msg)
         
     def controller_callback(self, msg):
+        if (msg.data == "start"): self.shouldTurn = False
+        elif (msg.data == "stop"): self.shouldMove = False
         self.get_logger().warn(f'The controller says I should {msg.data} the turtle ...')
 
 
